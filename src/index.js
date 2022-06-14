68b09fe1730277db6c85f7857051749b1864754f
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-06-14 16:05:51 +0300

    модалка в контактах

--- src/index.js
+++ src/index.js
@@ -65,3 +65,20 @@
   }
 })();
 
+(() => {
+  const refs = {
+    openFranchiseBtn: document.querySelector(".franchise-modal"),
+    closeFranchiseBtn: document.querySelector(".franchise-modal-close-btn"),
+    franchise: document.querySelector(".franchise-backdrop"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openFranchiseBtn.addEventListener("click", toggleMenu);
+  refs.closeFranchiseBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.franchise.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
+
