7eab5348c8ba83a6e5e0b021212c1671a75d3efb
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-06-14 11:57:02 +0000

    Deploying to gh-pages from @ PavelBazilevich/Seven_up_project_ice_cream@d955ce517ef14d9800a6a40732edd741c315c480 🚀

---
+++ index.398028ea.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){!function(){var n=this,o=$(".back-to-top");$(window).on("scroll",(function(){$(n).scrollTop()>=300?o.fadeIn():o.fadeOut()})),o.on("click",(function(n){n.preventDefault(),$("html").animate({scrollTop:0},1e3)}))}()}));
+//# sourceMappingURL=index.398028ea.js.map
